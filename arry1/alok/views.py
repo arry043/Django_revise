292d8562d50ad9d6848c50cbfc0ee587c851feb5
Arry <arifquerry@gmail.com>
2026-01-15 16:11:17 +0530

    first commit

---
+++ arry1/alok/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def all_alok(request):
+    return render(request, 'alok/all_alok.html') 
---
+++ arry1/arry1/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("<h1>Home Page</h1>") 
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>") 
+
+def contact(request):
+    return HttpResponse("<h1>Contact Page</h1>") 
+
+def service(request):
+    return HttpResponse("<h1>Service Page</h1>") 
+
+def blog(request):
+    return HttpResponse("<h1>Blog Page</h1>") 
