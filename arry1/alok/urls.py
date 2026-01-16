c78450ef40036ad4d87fb0606eaac4cdd50fdc42
Arry <arifquerry@gmail.com>
2026-01-16 16:15:12 +0530

    done

--- arry1/alok/urls.py
+++ arry1/alok/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_alok, name='all_alok'),
+    path('<int:id>/', views.alok_detail, name='alok_detail'),
 ]
--- arry1/alok/views.py
+++ arry1/alok/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import AlokVarity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -7,3 +8,7 @@ def all_alok(request):
     
     aloks = AlokVarity.objects.all()
     return render(request, 'alok/all_alok.html', {'aloks':aloks})
+
+def alok_detail(request, id):
+    alok = get_object_or_404(AlokVarity, pk=id)
+    return render(request, 'alok/alok_detail.html', {'alok':alok})
